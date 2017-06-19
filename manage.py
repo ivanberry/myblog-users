03363ce159eb5f3ddb5961d3d443463298a7618c
ivanberry <tabvim.dev@gmail.com>
2017-06-20 06:06:10 +0800

    [Add]:coverage

--- manage.py
+++ manage.py
@@ -1,9 +1,21 @@
 # manage.py
 
+import unittest
+import coverage
+
 from flask_script import Manager
 from project import create_app, db
-import unittest
 from project.api.models import User
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*'
+    ]
+)
+
+COV.start()
 
 app = create_app()
 manager = Manager(app)
@@ -31,6 +43,26 @@ def seed_db():
     db.session.add(User(username='tab', email='tab@gmail.com'))
     db.session.add(User(username='shirting', email='shirting@gmail.com'))
     db.session.commit()
+
+@manager.command
+def cov():
+    '''Run the unit tests with coverage.'''
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessfull():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary: ')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
+
+
+
+
+
 
 
 if __name__ == '__main__':
