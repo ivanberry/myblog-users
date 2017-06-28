dc1f77a741fc9b6809aa3357de1e22d25ed2327f
ivanberry <tabvim.dev@gmail.com>
2017-06-28 21:17:18 +0800

    [Add]:Bcrypt log

--- project/tests/test_config.py
+++ project/tests/test_config.py
@@ -24,6 +24,7 @@ class TestDevelopmentConfig(TestCase):
             app.config['SQLALCHEMY_DATABASE_URI'] ==
             os.environ.get('DATABASE_URL')
         )
+        self.assertTrue(app.config['BCRYPT_LOG_ROUNDS'] == 4)
 
 
 class TestTestingConfig(TestCase):
@@ -41,7 +42,7 @@ class TestTestingConfig(TestCase):
             ==
             os.environ.get("DATABASE_TEST_URL")
         )
-
+        self.assertTrue(app.config['BCRYPT_LOG_ROUNDS'] == 4)
 
 class TestProductionConfig(TestCase):
 
@@ -53,7 +54,7 @@ class TestProductionConfig(TestCase):
         self.assertTrue(app.config['SECRET_KEY'] is 'weak_full_stack')
         self.assertFalse(app.config['DEBUG'])
         self.assertFalse(app.config['TESTING'])
-
+        self.assertTrue(app.config['BCRYPT_LOG_ROUNDS'] == 13)
 
 if __name__ == '__main__':
     unittest.main()
